9c88f21025ecfd2741eaea7c7472063f3955a16f
Erin Millard <ezzatron@gmail.com>
2017-12-17 17:21:11 +1000

    Use a formula for frequency.

--- src/components/string-set-tensions.jsx
+++ src/components/string-set-tensions.jsx
@@ -2,23 +2,55 @@ import React from 'react'
 
 import {stringTension} from '../math'
 
+const SEMITONE_RATIO = Math.pow(2, 1/12)
+
+/**
+ * If I knew more math, I would make this better :(
+ */
+function frequency (offset) {
+  let f = 440
+
+  if (offset === 0) return f
+
+  if (offset < 0) {
+    for (let i = offset; i < 0; ++i) {
+      f /= SEMITONE_RATIO
+    }
+  } else {
+    for (let i = 0; i < offset; ++i) {
+      f *= SEMITONE_RATIO
+    }
+  }
+
+  return f
+}
+
 export default function StringSetTensions (props) {
   const {set} = props
   const {strings} = set
 
+  const scale = [
+    25.5,
+    25.5,
+    25.5,
+    25.5,
+    25.5,
+    25.5,
+  ]
+
   const tuning = [
-    329.628,
-    246.942,
-    195.998,
-    146.832,
-    110.000,
-    82.4069
+    frequency(-5),
+    frequency(-10),
+    frequency(-14),
+    frequency(-19),
+    frequency(-24),
+    frequency(-29)
   ]
 
   const rows = strings.map(({gauge, weight}, index) => {
     return <tr key={index}>
       <td>{gauge}</td>
-      <td>{stringTension(weight, 25.5, tuning[index]).toFixed(3)}</td>
+      <td>{stringTension(weight, scale[index], tuning[index]).toFixed(3)}</td>
     </tr>
   })
 
